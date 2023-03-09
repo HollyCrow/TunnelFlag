1fdd51e67afd3ee4f9acb120738182f814370b6d
holly <>
2023-03-09 14:27:18 +0000

    More includes in main + start printf

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+#include "game/game.h"
+#include "utils/keyboard.h"
+
 using namespace std;
 
 int screen_width = 1280;
@@ -14,23 +17,11 @@ SDL_Renderer* renderer = nullptr;
 
 
 int main(){
-
+    printf("Starting TunnelFlag...");
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(screen_width, screen_height, 0, &window, &renderer);
     SDL_RenderSetScale(renderer,1,1);
 
-
-    bool running = true;
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    running = false;
-                    break;
-            }
-        }
-    }
 
     return 0;
 }
