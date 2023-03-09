6775d303adefe39a07c36f72f508510ea62d3ffe
microwavedram <george.d.forrest@outlook.com>
2023-03-09 17:46:56 +0000

    more shite

--- client/src/utils/color.cpp
+++ client/src/utils/color.cpp
@@ -5,7 +5,7 @@
 #include "color.h"
 #include <cstdint>
 
-Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) {
     this->a = a;
     this->r = r;
     this->g = g;
