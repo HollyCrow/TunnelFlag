2d9ed6f0efdb5fadf93d579fcc3f1241edc99eb9
Bumpkin-Pi <h.person.notrobot@gmail.com>
2023-03-09 14:33:34 +0000

    Created camera class

---
+++ client/src/camera/camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by holly on 09/03/23.
+//
+
+#include "camera.h"
---
+++ client/src/camera/camera.h
@@ -0,0 +1,8 @@
+//
+// Created by holly on 09/03/23.
+//
+
+#ifndef TUNNELFLAG_CAMERA_H
+#define TUNNELFLAG_CAMERA_H
+
+#endif //TUNNELFLAG_CAMERA_H
