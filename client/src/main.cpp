9be7beee87a7fbfb3ce81eafd721cbee5f494bc6
Bumpkin-Pi <h.person.notrobot@gmail.com>
2023-03-09 16:03:48 +0000

    Added quit function and window tittle

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -14,15 +14,20 @@ int screen_width = 1280;
 int screen_height = 960;
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
+Game game(0, Vector2(1, 1));
 
 
+void quit(){
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 int main(){
     printf("Starting TunnelFlag...");
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(screen_width, screen_height, 0, &window, &renderer);
     SDL_RenderSetScale(renderer,1,1);
-
+    SDL_SetWindowTitle(window, "Tunnel Flag");
 
     return 0;
 }
