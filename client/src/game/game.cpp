7f4434eb0762e0a9eee2733fa29c7c959883b8fc
Bumpkin-Pi <h.person.notrobot@gmail.com>
2023-03-09 12:46:58 +0000

    Added game class

---
+++ client/src/game/game.cpp
@@ -0,0 +1,18 @@
+//
+// Created by holly on 09/03/23.
+//
+
+#include "game.h"
+#include "../utils/vector2.h"
+
+
+Game::Game(int ip, Vector2 map_dimensions){
+    this->width = map_dimensions.x;
+    this->height = map_dimensions.y;
+    this->ip = ip;
+    this->local_player = Player(Vector2(0, 0), true);
+}
+
+void Game::update_positions(){
+    this->local_player.update_position();
+}
