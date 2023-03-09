9675e27bd7b74883fb41ee9fb6d65122a5236d8e
holly <>
2023-03-09 18:24:11 +0000

    Semi broken static threaded rendering

--- client/src/camera/camera.cpp
+++ client/src/camera/camera.cpp
@@ -4,48 +4,62 @@
 
 #include "camera.h"
 #include <SDL2/SDL.h>
+#include <thread>
+
 
 #include "../game/game.h"
 #include "../utils/vector2.h"
 
-extern SDL_Window* window;
-extern SDL_Renderer* renderer;
+using namespace std;
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
 
 extern Game game;
 extern int screen_width;
 extern int screen_height;
 
 
-
-Camera::Camera(){
+Camera::Camera() {
 
 }
 
 
 void Camera::draw_game() {
-//    game.local_player.velocity.x = 0.1;
-
-    for (int i = 0; i < 5000; i++){
-        Vector2 camera_center(screen_width/2, screen_height/2);
-        Vector2 modifier = camera_center.get_minus(game.local_player.position.get_multiple(this->scale));
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, game.local_player.color.r, game.local_player.color.g, game.local_player.color.b, game.local_player.color.a);
-        SDL_Rect p; // Player rectangle, replace with texture for player sprite to not be rectangle
-        p.x = game.local_player.position.x + modifier.x - game.local_player.scale.x/2;
-        p.y = game.local_player.position.y + modifier.y - game.local_player.scale.y/2;
-        p.w = game.local_player.scale.x;
-        p.h = game.local_player.scale.y;
-        SDL_RenderFillRect(renderer, &p);
-        SDL_RenderPresent(renderer);
-//        game.local_player.update_position();
-    }
-
+    Vector2 modifier;
+    modifier = Vector2(screen_width / 2, screen_height / 2);
+    modifier.minus(game.local_player.position.get_multiple(this->scale));
+    printf(" %f ", (modifier.x));
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, game.local_player.color.r, game.local_player.color.g, game.local_player.color.b,
+                           game.local_player.color.a);
+    SDL_Rect p; // Player rectangle, replace with texture for player sprite to not be rectangle
+    p.x = game.local_player.position.x + modifier.x - (game.local_player.scale.x / 2);
+    p.y = game.local_player.position.y + modifier.y - (game.local_player.scale.y / 2);
+    p.w = (game.local_player.scale.x);
+    p.h = (game.local_player.scale.y);
+    SDL_RenderFillRect(renderer, &p);
+    SDL_RenderPresent(renderer);
 }
-
 
 void Camera::rescale_window(int w, int h) {
     SDL_SetWindowSize(window, w, h);
     screen_height = h;
     screen_width = w;
 }
+
+//static void render_thread(Camera *camera) {
+//    while (!camera->close) {
+//        camera->draw_game();
+//    }
+//}
+
+//void Camera::start_camera() {
+//    std::thread render_thread_object(&render_thread, this);
+//}
+
+//void Camera::stop_camera() {
+//    close = true;
+//}
+
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <thread>
 
 #include "game/game.h"
 #include "utils/keyboard.h"
@@ -15,27 +16,38 @@ int screen_height = 960;
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 Game game(0, Vector2(1, 1));
-
+//std::thread render_thread_object;
 
 void quit(){
+//    std::terminate(render_thread_object);
+
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
+void render_thread(Camera *camera) {
+    while (!camera->close) {
+        camera->draw_game();
+    }
+}
+
+
+
 int main(){
-    printf("Starting TunnelFlag...");
+    printf("\n - Tunnel Flag - \n\n");
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(screen_width, screen_height, 0, &window, &renderer);
     SDL_RenderSetScale(renderer,1,1);
-    SDL_SetWindowTitle(window, "Tunnel Flag");
+    SDL_SetWindowTitle(window, "\n - Tunnel Flag - \n");
     game.local_player.set_position(Vector2(100, 100));
     game.local_player.scale = Vector2(100, 100);
     Camera camera;
-    camera.draw_game();
-//    camera.rescale_window(400, 400);
-//    camera.draw_game();
+    std::thread render_thread_object(&render_thread, &camera);
+//    std::terminate(render_thread_object);
+    SDL_Delay(5000);
+    camera.close = true;
+    SDL_Delay(5000);
 
-    //SDL_Delay(3000);
 
     return 0;
 }
