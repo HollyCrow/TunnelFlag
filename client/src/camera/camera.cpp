c72b9ce0f92e579e7e57af9edab7e49d93f61562
holly <>
2023-03-10 15:17:16 +0000

    Player movement and camera cleared up more

--- client/src/camera/camera.cpp
+++ client/src/camera/camera.cpp
@@ -33,8 +33,8 @@ void Camera::draw_players(Vector2 offset) {
     SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
     for (int p = 0; p < game.player_number; p++){
         set_color(game.players[p].color);
-        rect.x = int(game.players[p].position.x + offset.x);
-        rect.y = int(game.players[p].position.y + offset.y);
+        rect.x = int(game.players[p].position.x + offset.x) - game.players[p].scale.x/2;
+        rect.y = int(game.players[p].position.y + offset.y) - game.players[p].scale.x/2;
         rect.w = int(game.players[p].scale.x);
         rect.h = int(game.players[p].scale.y);
         SDL_RenderFillRect(renderer, &rect);
@@ -43,8 +43,8 @@ void Camera::draw_players(Vector2 offset) {
 
 void Camera::draw_local_player(Vector2 offset) {
     set_color(game.local_player.color);
-    rect.x = int(game.local_player.position.x + offset.x);
-    rect.y = int(game.local_player.position.y + offset.y);
+    rect.x = int(game.local_player.position.x + offset.x) - game.local_player.scale.x/2;
+    rect.y = int(game.local_player.position.y + offset.y) - game.local_player.scale.y/2;
     rect.w = int(game.local_player.scale.x);
     rect.h = int(game.local_player.scale.y);
     SDL_RenderFillRect(renderer, &rect);
@@ -62,48 +62,16 @@ void Camera::draw_background(Vector2 offset) {
 
 
 void Camera::draw_game() {
-    Vector2 offset(-game.local_player.position.x, -game.local_player.position.y);
+    Vector2 offset(-game.local_player.position.x, -game.local_player.position.y); //I just spent 20+ minutes trying to figure out why the player was in the wrong place, without realising I was setting player to pos 100,100 in main.cpp
     offset.add(screen_width/2, screen_height/2);
-    offset.minus(game.local_player.scale.x/2, game.local_player.scale.y/2);
     SDL_SetRenderDrawColor(renderer,0, 0, 0, 255);
     SDL_RenderClear(renderer);
-
 
     draw_background(offset);
     draw_players(offset);
     draw_local_player(offset);
 
-
-//    SDL_RenderCopy( renderer, background_texture, NULL, &p );
-
-
-
     SDL_RenderPresent(renderer);
-
-
-
-
-
-
-////    printf(" %f ", game.local_player.position.x);
-//    Vector2 modifier;
-//    modifier = Vector2(screen_width / 2, screen_height / 2);
-//    modifier.minus(game.local_player.position.get_multiple(this->scale));
-////    printf(" %f ", (modifier.x));
-//    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-//    SDL_RenderClear(renderer);
-//    SDL_SetRenderDrawColor(renderer,
-//                           game.local_player.color.r,
-//                           game.local_player.color.g,
-//                           game.local_player.color.b,
-//                           game.local_player.color.a);
-//    SDL_Rect p; // Player rectangle, replace with texture for player sprite to not be rectangle
-//    p.x = game.local_player.position.x + modifier.x - (game.local_player.scale.x / 2)*scale;
-//    p.y = game.local_player.position.y + modifier.y - (game.local_player.scale.y / 2)*scale;
-//    p.w = (game.local_player.scale.x)*scale;
-//    p.h = (game.local_player.scale.y)*scale;
-//    SDL_RenderFillRect(renderer, &p);
-//    SDL_RenderPresent(renderer);
 }
 
 void Camera::rescale_window(int w, int h) {
