19edfe8a34e73b22ac1d0a564127a623470d2cd4
holly <>
2023-03-16 17:22:11 +0000

    Movement reimplemented

--- client/src/game/game.cpp
+++ client/src/game/game.cpp
@@ -3,21 +3,25 @@
 //
 
 #include "game.h"
+#include "../utils/keyboard.h"
 #define PORT 25567
 #include <utility>
 
 
+extern Keyboard keyboard;
+
+
 Game::Game() {
-    ServerInfo defaultServer;
-    defaultServer.port = PORT;
-    defaultServer.address = 2130706433;
-    defaultServer.name = "A server";
-
-    Map defaultMap;         //Aqua-man drowns in endgame
-    defaultMap.width = 0;
-    defaultMap.height = 0;
-
-    Game(defaultServer, defaultMap, 1, 0);
+//    ServerInfo defaultServer;
+//    defaultServer.port = PORT;
+//    defaultServer.address = 2130706433;
+//    defaultServer.name = "A server";
+//
+//    Map defaultMap;         //Aqua-man drowns in endgame
+//    defaultMap.width = 0;
+//    defaultMap.height = 0;
+//
+//    Game(defaultServer, defaultMap, 1, 0);
 }
 
 Game::Game(ServerInfo serverInfo, Map map, int number_of_players, int localPlayerID) {
@@ -30,3 +34,12 @@ Game::Game(ServerInfo serverInfo, Map map, int number_of_players, int localPlaye
 void Game::ClickEvent() {
 
 }
+
+void Game::update() {
+    players[localPlayerID].position.add(keyboard.player_move);
+//    for (int p = 0; p < player_number; p++) {
+//        players[p].update_position();
+//    }
+}
+
+
